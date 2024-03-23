49ccb460caf04794fa435d5c969200004d2355e8
ztttaishiji <ztttaishiji@163.com>
2024-03-23 23:46:27 +0800

    ssmp结束

--- springboot_08_ssmp/src/main/java/com/itheima/service/IBookService.java
+++ springboot_08_ssmp/src/main/java/com/itheima/service/IBookService.java
@@ -3,11 +3,21 @@ package com.itheima.service;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.itheima.domain.Book;
+import org.apache.ibatis.annotations.Insert;
 
 public interface IBookService extends IService<Book> {
     //添加非通用操作API接口
 
 
 
+
+    boolean saveBook(Book book);
+    boolean modify(Book book);
+    boolean delete(Integer id);
+
+
     IPage<Book> getPage(int currentPage,int pageSize);
+
+    IPage<Book> getPage(int currentPage, int pageSize, Book book);
+
 }
